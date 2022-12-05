e1ea9a859e4715eff280ff2a38b3376b1166f614
premrajkumar <premraj.pk77@gmail.com>
2022-12-05 16:24:26 +0530

    1st cmt

---
+++ src/test/java/jenkins_practice/BuildForprojectTest.java
@@ -0,0 +1,18 @@
+package jenkins_practice;
+
+import org.testng.annotations.Test;
+
+public class BuildForprojectTest {
+
+	@Test
+	public void main()
+	{
+		System.out.println("1st Build");
+	}
+	
+	@Test
+	public void main1()
+	{
+		System.out.println("2nd Build");
+	}
+}
